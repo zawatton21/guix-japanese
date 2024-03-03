d113eac04f948662aee9c6b9ac330a29ac8f2718
zawatton21 <kurozawawo@gmail.com>
2024-03-03 16:21:54 +0900

    2024-03-03_16:21:53

--- my-guix-channel/packages/ibus.scm
+++ my-guix-channel/packages/ibus.scm
@@ -186,7 +186,7 @@ standard library.")
                                 ;; 依存関係のパスを定義
                                 (define unzip-bin (string-append (assoc-ref %build-inputs "unzip") "/bin"))
                                 (define bash-bin (string-append (assoc-ref %build-inputs "bash") "/bin"))
-                                (define coreutils-bin (string-append (assoc-ref %build-inputs "coreutils) "/bin"))
+                                (define coreutils-bin (string-append (assoc-ref %build-inputs "coreutils") "/bin"))
                                 (define findutils-bin (string-append (assoc-ref %build-inputs "findutils") "/bin"))
                                 (define grep-bin (string-append (assoc-ref %build-inputs "grep") "/bin"))
                                 (define sed-bin (string-append (assoc-ref %build-inputs "sed") "/bin"))
