2bd606ea26b092a6617b3c6058acd787bbbb787c
zawatton21 <kurozawawo@gmail.com>
2024-03-04 22:32:42 +0900

    2024-03-04_22:32:41

--- guix-japanese/packages/fcitx5.scm
+++ guix-japanese/packages/fcitx5.scm
@@ -221,7 +221,7 @@
                                 (lambda _ 
                                   (substitute* "CMakeLists.txt"
                                                (("option\\(ENABLE_QT \"Enable Qt for GUI configuration\" On\\)")
-                                                "option(ENABLE_QT \"Enable Qt for GUI configuration\" On)"))
+                                                "option(ENABLE_QT \"Enable Qt for GUI configuration\" Off)"))
                                   (substitute* "CMakeLists.txt"
                                                (("option\\(USE_QT6 \"Build against Qt6\" On\\)")
                                                 "option(USE_QT6 \"Build against Qt6\" Off)"))
